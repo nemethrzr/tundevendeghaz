9cc7c9c636202b499142f6c409873b04f95f4c56
Nemeth Laszlo <nemeth.rzr@gmail.com>
2017-09-20 17:34:14 +0200

    képek hozzáadva a projekthez, galériához

--- menu/fooldal.php
+++ menu/fooldal.php
@@ -43,20 +43,18 @@
 <h2>Az oldal fejlesztés/karbantartás alatt van, addig is szíves elnézését, és türelmét kérjük!</h2>
 
 <h2>Üdvözli Önt Tiszakécskén a Tünde Vendégház!</h2>
-<p>
+<p>A nagy érdeklődés miatt foglalja le időben szállását. Árajánlataink a teljes vendégház foglalására vonatkoznak. Vegye igénybe egyéb szolgáltatásainkat is.</p>
 
-A nagy érdeklődés miatt foglalja le időben szállását. Árajánlataink a teljes vendégház foglalására vonatkoznak. Vegye igénybe egyéb szolgáltatásainkat is.
+<h4>Elhelyezkedés:</h4>
+     <p>A Tünde Vendégház,Tiszakécskén a termálfürdő közelében, kellemes környezetben, áprilistól - októberig várja vendégeit. Legyen szó pihenésről, városnézésről, vagy akár üzleti útról, válasszon bennünket. </p>
+     <p>Vendégházunk Tiszakécske üdülő körzetében a Nyúl u 26 sz. alatt található. Mindössze két utcával a termálfürdőtől (kb 800 méterre), valamint a Tisza szabad strandjától ( kb 1000 méterre) található.</p>
+<p>A szállás közelében étterem, pizzéria, fagylaltozó, ABC.</p>
+<p>A buszmegálló közvetlen a strand előtt van.</p>
+<p>Bank automaták a városközpontban a körforgalomnál. </p>
+<p>Spar, Penny, Coop üzletek, Posta a város kivezető szakaszán megközelíthetők.</p>
+<p>Mol benzinkút a szolnoki úton található. </p>
 
-Elhelyezkedés:
-     A Tünde Vendégház,Tiszakécskén a termálfürdő közelében, kellemes környezetben, áprilistól - októberig várja vendégeit. Legyen szó pihenésről, városnézésről, vagy akár üzleti útról, válasszon bennünket. 
-     Vendégházunk Tiszakécske üdülő körzetében a Nyúl u 26 sz. alatt található. Mindössze két utcával a termálfürdőtől (kb 800 méterre), valamint a Tisza szabad strandjától ( kb 1000 méterre) található.
-A szállás közelében étterem, pizzéria, fagylaltozó, ABC. 
-A buszmegálló közvetlen a strand előtt van. 
-Bank automaták a városközpontban a körforgalomnál. 
-Spar, Penny, Coop üzletek, Posta a város kivezető szakaszán megközelíthetők.
-Mol benzinkút a szolnoki úton található. 
 
-</p>
 <p><b>Szerezzen felejthetetlen élményeket nálunk! Kérjen ajánlatot, és foglalja le szállását időben!</b></p>
 
 	</div>
--- menu/galeria.php
+++ menu/galeria.php
@@ -58,6 +58,170 @@
 
 </section>
 
+<!-- 2-es row -->
+ <section class="row">
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="img/WP_20160909_012.JPG" title="" class="zoom" data-lightbox="images" data-title="sd" data-footer="sd" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160909_012.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                
+            </div>
+             <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/IMG_0250.JPG" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/IMG_0250.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+             <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160912_004.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_004.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="img/WP_20160909_007.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160909_007.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/IMG_0241.JPG" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/IMG_0241.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160909_013.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160909_013.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160912_001.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_001.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160912_003.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_003.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/IMG_0245.JPG" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/IMG_0245.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160909_008.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160909_008.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/IMG_0249.JPG" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/IMG_0249.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+             <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160912_002.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_002.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">                    
+                    <a href="img/WP_20160912_006.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_006.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+               
+            </div>
+        </article>
+
+        <article class="col-xs-12 col-sm-6 col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="img/IMG_0242.JPG" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/IMG_0242.JPG" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="img/WP_20160909_014.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160909_014.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                
+            </div>
+             <div class="panel panel-default">
+                <div class="panel-body">
+                    <a href="img/WP_20160912_005.jpg" title="" class="zoom" data-lightbox="images" data-title="" data-footer="" data-type="image" data-toggle="lightbox">
+                        <img src="img/WP_20160912_005.jpg" alt="" />
+                        <span class="overlay"><i class="glyphicon glyphicon-fullscreen"></i></span>
+                    </a>
+                </div>
+                
+            </div>
+        </article>                                              
+
+</section>
+
+
 
 
 
