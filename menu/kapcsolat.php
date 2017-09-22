b528b5c132f7e03c9f054d8b1935a4ba137b315a
Nemeth Laszlo <nemeth.rzr@gmail.com>
2017-09-22 21:06:17 +0200

    kapcsolat - email form hibaüzenetek

--- menu/kapcsolat.php
+++ menu/kapcsolat.php
@@ -1,3 +1,5 @@
+
+
 <div class="container">
 <div class="content">
      
@@ -36,26 +38,69 @@
            <div class="row">
            		<div class="col-md-3"></div>
 	           	<div class="col-md-6">
+
+
+						<?php
+					$alertInputName   = '';
+					$alertInputEmail  = '';
+					$alertInputText   = '';
+					$success = '<p class="alert alert-success">Email elküldve!</p>';
+					$bool  = false;
+					if(isset($_POST['btnSend'])){
+						
+						if($_POST['inputName']==null){
+							$alertInputName = '<p class="alert alert-danger">Nem adta meg a nevét!</p>';
+							$bool = true;
+
+						}
+						if($_POST['inputEmail']==null){
+							$alertInputEmail = '<p class="alert alert-danger">Nem adta meg az email címét</p>';
+							$bool = true;
+						}
+						if($_POST['inputText']==null){
+							$alertInputText .= '<p class="alert alert-danger">A szöveg mező nem lehet üres</p>';
+							$bool = true;
+						}
+						if(!$bool){
+							//email elküldése
+							echo $success;
+
+						}
+						
+
+					}
+
+					?>
 		           	<h3>Üzenet küldése: </h3>
-		           	<form>
+		           	<form method="post" action="kapcsolat" name="formEmail">
 					  <div class="form-group">
 					    <label for="formGroupExampleInput">Feladó:</label>
 					    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Feladó neve" name="inputName">
+					    <div class="form-group"><?php echo $alertInputName ?></div>
 					  </div>
 					  <div class="form-group">
 					  	<label for="formGroupExampleInput3">Email:</label>
 					  	<input type="email" class="form-control" id="formGroupExampleInput3" placeholder="Email" name="inputEmail">
+					  	<div class="form-group"><?php echo $alertInputEmail ?></div>
 					  </div>
 					  <div class="form-group">
 					    <label for="formGroupExampleInput2">Üzenet</label>
 					    
 					    <textarea class="form-control" id="formGroupExampleInput2" placeholder="Email szövege" rows="7" name="inputText"></textarea>
+					    <div class="form-group"><?php echo $alertInputText ?></div>
 					  </div>
 					  <div class="form-group">				    
 					    <input type="submit" class="btn btn-primary" id="formGroupExampleInput2" title="Küldés" name="btnSend">
 					  </div>
 					</form>
+
+
+					
 				</div>
+
+
+
+			
 				<div class="col-md-3"></div>
            </div>
     </div>
